09e601ec52f44bda6f5e796c898ea2241f84d64b
Ted Goldstein <ted@leadable.com>
2015-06-03 13:40:02 -0400

    calc md5 of view and materialized view while generating sql diff, refactor a bit to support this

--- beaches/lib/MLS/Property/Publish.pm
+++ beaches/lib/MLS/Property/Publish.pm
@@ -6,6 +6,8 @@ use warnings;
 $| = 1;
 
 use Data::Dumper qw(Dumper);
+use Digest::MD5 qw(md5_hex);
+use Mojo::JSON qw(j);
 
 sub new {
   my ($class, $opts) = @_;
@@ -21,6 +23,16 @@ sub go {
     print "----Generate Publish Data---\n\n";
     $self->{id_lists} = {new => [], updated => []};
     $self->{totals} =   {new => 0,  updated => 0};
+
+    # get data from view and materialized view
+    $self->get_data;
+
+    # compare md5 from both tables to see if this should proceed
+    if ($self->{view_md5} eq $self->{live_md5}) {
+        print "Tables are equivalent, no need to continue\n";
+        $self->finish();
+        return;
+    }
   
     # Compare key columns of view with materialized view data
     $self->build_id_lists;
@@ -31,6 +43,9 @@ sub go {
     # dump .sql file to somewhere
     open(my $fh, '>', $self->{sql_file_out}) or
        die "Could not open [$self->{sql_file_out}] for write: $!";
+
+    my $md5_json = j({old => $self->{live_md5}, new => $self->{view_md5}});
+    print $fh "--$md5_json\n";
     print $fh $sql_to_write;
 
     # recreate materialized view from our new view
@@ -41,28 +56,49 @@ sub go {
 sub finish {
   my $self = shift;
 
+  $self->{totals}{new}     = scalar (@{$self->{id_lists}{new}});
+  $self->{totals}{updated} = scalar (@{$self->{id_lists}{updated}});
+
   print "\nReport:\n";
   print Dumper $self->{totals};
   print "Wrote SQL diff to [$self->{sql_file_out}]\n";
   print "\n[DONE]\n\n";
 }
 
-sub build_id_lists {
+sub get_data {
     my $self = shift;
 
     my $dbh = $self->{dbh};
 
-    my @key_cols = qw(__modified_at __removed_at);
+    my @key_cols = qw(__modified_at __removed_at __inserted_at);
     my $cols_str =
         join ',',
         map {"extract(epoch from $_) as $_"} @key_cols;
 
-    my $view_rs = $dbh->selectall_hashref("SELECT $cols_str,listing_id from $MLS::Property::Config::MLS.view_listings", 'listing_id');
-    my $live_rs = $dbh->selectall_hashref("SELECT $cols_str,listing_id from $MLS::Property::Config::MLS.view_listings_materialized", 'listing_id');
+    my $view_rs = $self->{view_rs} = $dbh->selectall_hashref("SELECT $cols_str,listing_id from $MLS::Property::Config::MLS.view_listings", 'listing_id');
+    my $live_rs = $self->{live_rs} = $dbh->selectall_hashref("SELECT $cols_str,listing_id from $MLS::Property::Config::MLS.view_listings_materialized", 'listing_id');
 
-    # Compare modified timestamps
     print scalar(keys %$view_rs) ." records in new view\n";
     print scalar(keys %$live_rs) ." records in live view\n";
+
+    # initialize potentially null values
+    foreach my $row_data (values %$view_rs, values %$live_rs) {
+        foreach my $row_val (values %$row_data) {
+            $row_val ||= 0;
+        }
+    }
+
+    $self->{view_md5} = do_md5sum_table($view_rs);
+    $self->{live_md5} = do_md5sum_table($live_rs);
+    print "view_md5: [$self->{view_md5}]\n";
+    print "live_md5: [$self->{live_md5}]\n";
+}
+
+sub build_id_lists {
+    my $self = shift;
+
+    my $view_rs = $self->{view_rs};
+    my $live_rs = $self->{live_rs};
 
     foreach my $id (keys %$view_rs) {
         # new listings
@@ -71,19 +107,26 @@ sub build_id_lists {
         }
         else {
             # modified, removed, resurrected all require updating the row
-
-            # initialize potentially null values
-            map {$view_rs->{$id}{$_} ||= 0; $live_rs->{$id}{$_} ||= 0; $_} @key_cols;
-
             if ($view_rs->{$id}{__modified_at} != $live_rs->{$id}{__modified_at} ||
                 $view_rs->{$id}{__removed_at}  != $live_rs->{$id}{__removed_at}) {
                 push @{$self->{id_lists}{updated}}, $id;
             }
         }
     }
+}
 
-    $self->{totals}{new}     = scalar (@{$self->{id_lists}{new}});
-    $self->{totals}{updated} = scalar (@{$self->{id_lists}{updated}});
+sub do_md5sum_table {
+    my $table_data = shift;
+
+    # sort by keys (listing id) here so data is always in same order
+    return md5_hex(
+        map {
+            $table_data->{$_}{listing_id} .
+            $table_data->{$_}{__inserted_at} .
+            $table_data->{$_}{__modified_at} .
+            $table_data->{$_}{__removed_at}
+        } sort keys %$table_data
+    );
 }
 
 sub generate_sql {
