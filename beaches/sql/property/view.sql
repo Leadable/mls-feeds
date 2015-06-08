5ada623a838f50506cdd31be195b9434c830f76b
Joe Ingersoll <joe.ingersoll@gmail.com>
2015-06-08 11:24:20 -0400

    no longer needed

--- beaches/sql/property/view.sql
+++
@@ -1,5 +0,0 @@
-DROP VIEW ragfl.view_property;
-
-CREATE OR REPLACE VIEW ragfl.view_property AS
-
-SELECT p.* FROM ragfl."Property" p JOIN ragfl.mutation m ON p.sysid::text = m.remote_id::text AND m.last_transaction_completed_at is not null;
