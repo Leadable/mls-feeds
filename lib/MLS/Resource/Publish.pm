02a2f0191b94e0562dd3f79e4a146983f03452aa
Ted Goldstein <ted@leadable.com>
2015-07-01 16:08:04 -0400

    Create a materialized view from our 'new data view' to make the publish process much faster
    Change a bunch of terminology to make this readable. 'view' and 'live' have been changed
    to 'new' and 'old' in the places where it applies

--- lib/MLS/Resource/Publish.pm
+++ lib/MLS/Resource/Publish.pm
@@ -24,20 +24,37 @@ sub go {
     print "----Generate Publish Data [$self->{id}]---\n\n";
     $self->{id_lists} = {new => [], updated => []};
 
+    # This is the name of our local view
+    # and also the table in the live db for this data
     $self->{view} = "$MLS::Config::MLS.view_$self->{id}";
-    $self->{materialized} = $self->{view} . '_materialized';
+
+    # these are the two sets of data that will be compared
+    $self->{new_data_materialized} = $self->{view} . '_new_materialized';
+    $self->{materialized}          = $self->{view} . '_materialized';
+
+    # create a materialized view of the new data so selecting is fast
+    print "Creating $self->{new_data_materialized}...\n";
+    $self->{dbh}->do(qq|
+        DROP MATERIALIZED VIEW IF EXISTS $self->{new_data_materialized};
+        CREATE MATERIALIZED VIEW $self->{new_data_materialized} as SELECT * from $self->{view};
+    |);
 
     # is the materialized table empty?
     if ($self->is_materialized_empty) {
+        print "Materialized table is empty, forcing rebuild\n";
         $self->{force_rebuild} = 1;
     }
 
     # determine if there is a schema change
     # if so, must rebuild the entire table on live
+    print "Checking for schema differences...\n";
     if ($self->is_schema_change || $self->{force_rebuild}) {
-        print "Schema change detected\n";
+        print "Full rebuild required\n";
 
         $self->{rebuild} = 1;
+
+        # live table is the name of the table on the live db where the data will be stored
+        # for rebuilds, store in a temporary table which is later replaced
         $self->{live_table} = $self->{view} . '_new';
     }
     else {
@@ -45,6 +62,7 @@ sub go {
     }
 
     # compare md5 of data from both tables to see if this should proceed
+    print "Checking for data differences...\n";
     if (!$self->is_data_change) {
         print "Tables are equivalent, no need to continue\n";
         $self->finish();
@@ -56,7 +74,7 @@ sub go {
 
     # compile list of instructions based on this
     my $sql_to_write = $self->generate_row_data;
-    $sql_to_write .= qq|COMMENT ON table $self->{live_table} is '$self->{view_data_md5}';|;
+    $sql_to_write .= qq|COMMENT ON table $self->{live_table} is '$self->{new_data_md5}';|;
 
     # add to our SQL diff if doing a full rebuild
     if ($self->{rebuild}) {
@@ -73,8 +91,13 @@ sub go {
 
     print "Refreshing materialized view...\n";
 
-    # recreate materialized view from our new view
-    $self->{dbh}->do("REFRESH MATERIALIZED VIEW $self->{materialized}");
+    # update the materialized view
+    $self->{dbh}->do(qq|
+        BEGIN;
+        DROP MATERIALIZED VIEW $self->{materialized};
+        ALTER MATERIALIZED VIEW $self->{new_data_materialized} RENAME TO view_$self->{id}_materialized;
+        COMMIT;
+    |);
 
     $self->finish();
 }
@@ -122,15 +145,15 @@ sub is_schema_change {
 
     my $dbh = $self->{dbh};
 
-    my $view_schema = $dbh->selectall_arrayref(qq|
+    my $new_schema = $dbh->selectall_arrayref(qq|
         SELECT attname, format_type(atttypid, atttypmod)
         FROM   pg_attribute
-        WHERE  attrelid = '$self->{view}'::regclass
+        WHERE  attrelid = '$self->{new_data_materialized}'::regclass
         AND    attnum > 0
         AND    NOT attisdropped;|
     );
 
-    my $live_schema = $dbh->selectall_arrayref(qq|
+    my $old_schema = $dbh->selectall_arrayref(qq|
         SELECT attname, format_type(atttypid, atttypmod)
         FROM   pg_attribute
         WHERE  attrelid = '$self->{materialized}'::regclass
@@ -138,17 +161,17 @@ sub is_schema_change {
         AND    NOT attisdropped;|
     );
 
-    $self->{view_schema_md5} = do_md5sum_schema($view_schema);
-    $self->{live_schema_md5} = do_md5sum_schema($live_schema);
+    $self->{new_schema_md5} = do_md5sum_schema($new_schema);
+    $self->{old_schema_md5} = do_md5sum_schema($old_schema);
 
     # format schema for publish later
-    $self->{view_schema} = [
+    $self->{new_schema} = [
         map {
             { col_name => $_->[0], col_type => $_->[1] }
-        } @$view_schema
+        } @$new_schema
     ];
 
-    return $self->{view_schema_md5} ne $self->{live_schema_md5};
+    return $self->{new_schema_md5} ne $self->{old_schema_md5};
 }
 
 sub is_data_change {
@@ -161,44 +184,44 @@ sub is_data_change {
         join ',',
         map {"extract(epoch from $_) as $_"} @key_cols;
 
-    my $view_rs = $self->{view_rs} = $dbh->selectall_hashref("SELECT $cols_str,id from $self->{view}", 'id');
+    my $new_rs = $self->{new_rs} = $dbh->selectall_hashref("SELECT $cols_str,id from $self->{new_data_materialized}", 'id');
 
-    my $live_rs = {};
+    my $old_rs = {};
     if (!$self->{rebuild}) {
-        $live_rs = $self->{live_rs} = $dbh->selectall_hashref("SELECT $cols_str,id from $self->{materialized}", 'id');
+        $old_rs = $self->{old_rs} = $dbh->selectall_hashref("SELECT $cols_str,id from $self->{materialized}", 'id');
     }
 
-    print scalar(keys %$view_rs) ." records in new view\n";
-    print scalar(keys %$live_rs) ." records in live view\n";
+    print scalar(keys %$new_rs) ." records in new view\n";
+    print scalar(keys %$old_rs) ." records in old view\n";
 
     # initialize potentially null values
-    foreach my $row_data (values %$view_rs, values %$live_rs) {
+    foreach my $row_data (values %$new_rs, values %$old_rs) {
         foreach my $row_val (values %$row_data) {
             $row_val ||= 0;
         }
     }
 
-    $self->{view_data_md5} = do_md5sum_data($view_rs);
-    $self->{live_data_md5} = do_md5sum_data($live_rs);
+    $self->{new_data_md5} = do_md5sum_data($new_rs);
+    $self->{old_data_md5} = do_md5sum_data($old_rs);
 
-    return $self->{view_data_md5} ne $self->{live_data_md5};
+    return $self->{new_data_md5} ne $self->{old_data_md5};
 }
 
 sub build_id_lists {
     my $self = shift;
 
-    my $view_rs = $self->{view_rs};
-    my $live_rs = $self->{live_rs};
+    my $new_rs = $self->{new_rs};
+    my $old_rs = $self->{old_rs};
 
-    foreach my $id (keys %$view_rs) {
+    foreach my $id (keys %$new_rs) {
         # new listings
-        if (! defined $live_rs->{$id}) {
+        if (! defined $old_rs->{$id}) {
             push @{$self->{id_lists}{new}}, $id;
         }
         else {
             # modified, removed, resurrected all require updating the row
-            if ($view_rs->{$id}{__modified_at} != $live_rs->{$id}{__modified_at} ||
-                $view_rs->{$id}{__removed_at}  != $live_rs->{$id}{__removed_at}) {
+            if ($new_rs->{$id}{__modified_at} != $old_rs->{$id}{__modified_at} ||
+                $new_rs->{$id}{__removed_at}  != $old_rs->{$id}{__removed_at}) {
                 push @{$self->{id_lists}{updated}}, $id;
             }
         }
@@ -238,7 +261,7 @@ sub generate_row_data {
     my @new_ids     = @{$self->{id_lists}{new}};
     my @updated_ids = @{$self->{id_lists}{updated}};
 
-    my @col_names = map {$_->{col_name}} @{$self->{view_schema}};
+    my @col_names = map {$_->{col_name}} @{$self->{new_schema}};
     my $col_str = join ',',
                   map {$dbh->quote_identifier($_)} @col_names;
 
@@ -251,7 +274,7 @@ sub generate_row_data {
 
             my $new_ids_str = join ' OR ',
                               map {"id = " . $dbh->quote($_)} @ids;
-            my $new_rs = $dbh->selectall_arrayref("SELECT * from $self->{view} where $new_ids_str", {Slice => {}});
+            my $new_rs = $dbh->selectall_arrayref("SELECT * from $self->{new_data_materialized} where $new_ids_str", {Slice => {}});
 
             foreach my $row (@$new_rs) {
                 my $vals = join ',',
@@ -273,7 +296,7 @@ sub generate_row_data {
 
             my $update_ids_str = join ' OR ',
                               map {"id = " . $dbh->quote($_)} @ids;
-            my $update_rs = $dbh->selectall_arrayref("SELECT * from $self->{view} where $update_ids_str", {Slice => {}});
+            my $update_rs = $dbh->selectall_arrayref("SELECT * from $self->{new_data_materialized} where $update_ids_str", {Slice => {}});
             $return_sql .= join "\n",
                            map {$self->format_row_data($_, $dbh)} @$update_rs;
             $return_sql .= "\n";
@@ -304,13 +327,13 @@ sub add_rebuild_sql {
 
     # build new table to house the data
     # later it will atomically replace current table
-    my $schema = $self->{view_schema};
+    my $schema = $self->{new_schema};
     my $index_sql = generate_index_sql($self, $schema, "view_$self->{id}");
     my $extra_sql = get_extra_sql($self->{id});
 
     my $cols = join ',',
                map {$self->{dbh}->quote_identifier($_->{col_name}) . ' ' . $_->{col_type}} @$schema;
-    my $new_table_sql = qq|CREATE TABLE $self->{view}_new ($cols);|;
+    my $new_table_sql = qq|CREATE TABLE $self->{live_table} ($cols);|;
 
     my $mv_table_sql = qq|DROP TABLE IF EXISTS $self->{view} CASCADE; ALTER TABLE $self->{live_table} RENAME TO "view_$self->{id}";|;
 
@@ -394,7 +417,7 @@ sub compress_sql {
     my $gz = gzopen($fh, 'wb9') or
         die "Could not open gzip file for write";
 
-    my $md5_json = j({old => $self->{live_data_md5}, new => $self->{view_data_md5}});
+    my $md5_json = j({old => $self->{old_data_md5}, new => $self->{new_data_md5}});
     $gz->gzwrite("--$md5_json\n");
     $gz->gzwrite($sql);
     die "there was a problem flushing [$filename]" if ($gz->gzclose);
@@ -449,8 +472,8 @@ sub insert_publish_table {
         area              => $self->{id},
         full              => $self->{rebuild} || '0',
         data_file_url     => $self->{data_file_url},
-        data_md5          => $self->{view_data_md5},
-        previous_data_md5 => $self->{live_data_md5},
+        data_md5          => $self->{new_data_md5},
+        previous_data_md5 => $self->{old_data_md5},
     );
 
     my @cols = map {$dbh->quote_identifier($_)} keys %row_data;
