e6b35691f2f8efc9fd89aaa0f6f9913290b5bd85
Ted Goldstein <ted@leadable.com>
2015-07-08 11:14:45 -0400

    add vendor to ragfl

--- ragfl2/lib/MLS/Config.pm
+++ ragfl2/lib/MLS/Config.pm
@@ -8,6 +8,9 @@ use Geo::StreetAddress::US;
 # MLS identifier
 $MLS::Config::MLS = 'ragfl';
 
+# Rets Vendor
+$MLS::Config::Vendor = 'Retsiq';
+
 # Location of logging
 $MLS::Config::LOG_DIR = "/tmp/log/$MLS::Config::MLS";
 
