202ce78ed5e1a51ad4c10951799fe7a4318e97bc
Ted Goldstein <ted@leadable.com>
2015-07-21 17:07:01 -0400

    mred: add areas

--- lib/MLS/Resource/Connectmls/mred/Config/Property.pm
+++ lib/MLS/Resource/Connectmls/mred/Config/Property.pm
@@ -6,7 +6,7 @@ use MLS::Resource::Connectmls::mred::Config;
 # ID of the Rets Property Resource
 $MLS::Config::RESOURCE = 'Property';
 
-@MLS::Config::AREAS = qw();
+@MLS::Config::AREAS = qw(bomba ebersole kelley mcdonnell);
 
 # Name of the RETS Resource Photo Object
 $MLS::Config::OBJECT = 'HrPhoto';
