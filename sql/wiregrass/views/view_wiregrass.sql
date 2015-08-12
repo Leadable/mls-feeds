d7c8c93200e4f0265f07b0a187c0929a49a1aa1b
Ted Goldstein <ted@leadable.com>
2015-08-12 10:58:43 -0400

    wiregrass: update view

--- sql/wiregrass/views/view_wiregrass.sql
+++ sql/wiregrass/views/view_wiregrass.sql
@@ -1,5 +1,7 @@
 DROP VIEW IF EXISTS wiregrass.view_wiregrass;
 CREATE OR REPLACE VIEW wiregrass.view_wiregrass AS
-  SELECT * 
-  FROM wiregrass.view_property
+  SELECT
+  91::integer as area_id,
+  vl.*
+  FROM wiregrass.view_property vl
 ;
