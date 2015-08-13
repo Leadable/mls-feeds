aff91e3019d3f2daff2a7441d652d26cc4c7a052
Ted Goldstein <ted@leadable.com>
2015-08-13 15:21:52 -0400

    wiregrass: add comments to view definition

--- sql/wiregrass/views/view_wiregrass.sql
+++ sql/wiregrass/views/view_wiregrass.sql
@@ -5,3 +5,23 @@ CREATE OR REPLACE VIEW wiregrass.view_wiregrass AS
   vl.*
   FROM wiregrass.view_property vl
 ;
+
+COMMENT ON COLUMN wiregrass.view_wiregrass.city 
+  IS '{ "location": true, "label": "City", "label_plural": "Cities", "rank": 10 }';
+COMMENT ON COLUMN wiregrass.view_wiregrass.subdivision 
+  IS '{ "location": true, "label": "Subdivision", "label_plural": "Subdivisions", "rank": 20 }';
+COMMENT ON COLUMN wiregrass.view_wiregrass.zip 
+  IS '{ "location": true, "label": "Zip", "label_plural": "Zip Codes", "rank": 40 }';
+COMMENT ON COLUMN wiregrass.view_wiregrass.elementary_school 
+  IS '{ "location": true, "label": "Elementary School", "label_plural": "Elementary Schools", "group": "Schools", "rank": 50 }';
+COMMENT ON COLUMN wiregrass.view_wiregrass.middle_school 
+  IS '{ "location": true, "label": "Middle School", "label_plural": "Middle Schools", "group": "Schools", "rank": 60 }';
+COMMENT ON COLUMN wiregrass.view_wiregrass.high_school 
+  IS '{ "location": true, "label": "High School", "label_plural": "High Schools", "group": "Schools", "rank": 70 }';
+
+COMMENT ON COLUMN wiregrass.view_wiregrass.basement IS '{ "feature": true, "label": "Basement" }';
+COMMENT ON COLUMN wiregrass.view_wiregrass.garage IS '{ "feature": true, "label": "Garage" }';
+COMMENT ON COLUMN wiregrass.view_wiregrass.fireplace IS '{ "feature": true, "label": "Fireplace" }';
+COMMENT ON COLUMN wiregrass.view_wiregrass.pool IS '{ "feature": true, "label": "Pool" }';
+COMMENT ON COLUMN wiregrass.view_wiregrass.patio_deck_porch IS '{ "feature": true, "label": "Patio/Deck/Porch" }';
+COMMENT ON COLUMN wiregrass.view_wiregrass.ranch_style IS '{ "feature": true, "label": "Ranch Style" }';
