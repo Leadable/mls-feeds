d87d8b8a43e72ecc86d02806988b8ba29536844f
Ted Goldstein <ted@leadable.com>
2015-08-14 12:08:40 -0400

    backups: store helpful metadata about backups in tools, only run backups daily at 2am local time

--- sql/bin/store_backup.pl
+++ sql/bin/store_backup.pl
@@ -6,11 +6,13 @@ use FindBin;
 use File::Temp qw(tempfile);
 use lib "$FindBin::Bin/../../lib";
 use MLS::Storage;
+use MLS::Database;
 
 die "BACKUP_FILE must be specified" if (!$ENV{BACKUP_FILE});
 die "MLS must be specified"         if (!$ENV{MLS_NAME});
 
 my $storage_client = MLS::Storage->new({ use_s3 => 0, bucket => 'db-backup'});
+my $dbh      = MLS::Database->new({db => 'tools'});
 
 my $filename = $ENV{BACKUP_FILE};
 my $mls      = $ENV{MLS_NAME};
@@ -25,6 +27,8 @@ open (my $fh, '<', $filename)
 # store file in 10M chunks
 my $buffer;
 my $i = 0;
+my @urls;
+
 while (read($fh, $buffer, 10000000)) {
 
     my ($part_fh, $part_filename) = tempfile(
@@ -38,12 +42,36 @@ while (read($fh, $buffer, 10000000)) {
     print $part_fh $buffer;
     close $part_fh;
 
-    my $url = $storage_client->store_file({
-      source_filename => $part_filename,
-      dest_filename   => "$mls/$time/$i.gz",
-      content_type    => 'text/plain',
-    });
+    my $url = eval {
+        $storage_client->store_file({
+          source_filename => $part_filename,
+          dest_filename   => "$mls/$time/$i.gz",
+          content_type    => 'text/plain',
+        });
+    };
+
+    if ($@) {
+        my $sql = qq|INSERT INTO backup VALUES ('$mls', NOW(), NULL, 'COULD NOT UPLOAD FILE [$mls/$time/$i.gz]');|;
+        $dbh->do($sql);
+        exit 1;
+    }
+
+    push @urls, $url;
 
     print "uploaded to [$url]\n";
     $i++;
 }
+
+my $sql;
+
+if (scalar @urls) {
+    my $sql_urls = 'ARRAY[' . join(',', map($dbh->quote($_), @urls)) . ']';
+    $sql = qq|INSERT INTO backup VALUES ('$mls', NOW(), $sql_urls, NULL);|;
+}
+else {
+    $sql = qq|INSERT INTO backup VALUES ('$mls', NOW(), NULL, 'NO FILES');|;
+}
+
+$dbh->do($sql);
+
+1;
