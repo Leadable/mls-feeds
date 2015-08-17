561c473565129e0e5ba660f1e7f8f11545fd333a
Ted Goldstein <ted@leadable.com>
2015-08-17 11:10:20 -0400

    beaches: deprecate __geo_neigh col

--- sql/beaches/views/view_kanga_realty.sql
+++ sql/beaches/views/view_kanga_realty.sql
@@ -3,25 +3,9 @@ CREATE OR REPLACE VIEW beaches.view_kanga_realty AS
   SELECT
    83::integer as area_id,
    vl.*,
-   neigh_places.label as __geo_neigh
+   null::text[] as __geo_neigh
   FROM
     (select * from beaches.view_property) vl
-    LEFT OUTER JOIN
-      (
-        SELECT * FROM
-          (
-            SELECT
-              vl.mlsnum AS mlsnum,
-              places.category as category,
-              array_agg(places.label) AS label
-            FROM
-              (select * from beaches.view_property) vl
-              JOIN (select * from beaches.places where area_id = 83 AND category = 'Neighborhood') as places ON ST_Contains(ST_SETSRID(places.way, 4326), vl.__geo_geom)
-            GROUP BY mlsnum, category
-          ) mls_places
-        WHERE category = 'Neighborhood'
-      )
-    neigh_places ON neigh_places.mlsnum = vl.mlsnum
 ;
 
 COMMENT ON COLUMN beaches.view_kanga_realty.city_st IS '{ "location": true, "label": "City", "label_plural": "Cities", "rank": 10 }';
