11d4497f6d093ad83441d90a28ca7c20dbdd296c
Ted Goldstein <ted@leadable.com>
2015-08-28 12:21:41 -0400

    mfrmls: add MV_ACTIVE_COLS to prioritize photos/geocode

--- lib/MLS/Resource/Matrix/mfrmls/Config/Property.pm
+++ lib/MLS/Resource/Matrix/mfrmls/Config/Property.pm
@@ -33,6 +33,29 @@ my $search = '(MatrixModifiedDT=' . MLS::Resource::Utils::get_search_interval() 
 # RETS Resource price column
 %MLS::Config::PRICE_COLUMN = ( SystemName => 'ListPrice' );
 
+# Four columns needed to make mv_active, listing_type, sold_date, __active, listing_id
+# These should be identical to the definitions in view_property
+$MLS::Config::MV_ACTIVE_COLS = q|
+  SELECT
+      CASE "Status"
+        WHEN 'Sold'   THEN 'sold'
+        WHEN 'Leased' THEN 'leased'
+        ELSE
+          CASE "PropertyType"
+            WHEN 'Rental' THEN 'for_rent'
+            ELSE
+              CASE "LeasePrice"
+                WHEN null THEN 'for_sale'
+                ELSE 'for_rent'
+              END
+          END
+      END as listing_type,
+      (__removed_at is null) as __active,
+      "Matrix_Unique_ID"::text as listing_id,
+      "CloseDate" as sold_date,
+  FROM mfrmls."Property"
+|;
+
 %MLS::Config::ADDR_COLUMNS = (
   street   => 'StreetName',
   number   => 'StreetNumber',
