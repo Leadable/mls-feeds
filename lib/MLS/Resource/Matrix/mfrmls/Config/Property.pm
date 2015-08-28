ab175432a031e70d521a04f4c23c20fdd3367fdd
Ted Goldstein <ted@leadable.com>
2015-08-28 17:09:36 -0400

    mfrmls add vernamonti area

--- lib/MLS/Resource/Matrix/mfrmls/Config/Property.pm
+++ lib/MLS/Resource/Matrix/mfrmls/Config/Property.pm
@@ -7,7 +7,7 @@ use MLS::Resource::Utils;
 # ID of the Rets Property Resource
 $MLS::Config::RESOURCE = 'Property';
 
-@MLS::Config::AREAS = qw(isner padula wade);
+@MLS::Config::AREAS = qw(isner padula wade vernamonti);
 
 # Name of the RETS Resource Photo Object
 $MLS::Config::OBJECT = 'LargePhoto';
