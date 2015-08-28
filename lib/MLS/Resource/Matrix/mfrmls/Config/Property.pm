ede30e488d25f41b1d951b2c77babf6f46351a69
Ted Goldstein <ted@leadable.com>
2015-08-28 16:25:12 -0400

    mfrmls sql syntax error

--- lib/MLS/Resource/Matrix/mfrmls/Config/Property.pm
+++ lib/MLS/Resource/Matrix/mfrmls/Config/Property.pm
@@ -46,7 +46,7 @@ $MLS::Config::MV_ACTIVE_COLS = q|
       END as listing_type,
       (__removed_at is null) as __active,
       "Matrix_Unique_ID"::text as listing_id,
-      "CloseDate" as sold_date,
+      "CloseDate" as sold_date
   FROM mfrmls."Property"
 |;
 
