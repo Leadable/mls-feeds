e6b9a99d06d54cbea52c57e367ed15668de4d3d6
Ted Goldstein <ted@leadable.com>
2015-10-13 14:33:21 -0400

    ntreis adjust row limit for PropertySubTable

--- lib/MLS/Resource/Matrix/ntreis2/Config/PropertySubTable.pm
+++ lib/MLS/Resource/Matrix/ntreis2/Config/PropertySubTable.pm
@@ -21,6 +21,6 @@ my $search = MLS::Resource::Utils::get_search_interval();
 );
 
 $MLS::Config::Mutation::OFFSET_SIZE = 100000;
-$MLS::Config::Row::OFFSET_SIZE      = 100000;
+$MLS::Config::Row::OFFSET_SIZE      = 50000;
 
 1;
