ea6ee96445ae3940e3045e9df69ccaa8923e56a3
Ted Goldstein <ted@leadable.com>
2016-03-10 13:13:54 -0500

    do not run sandicor during offpeak hours due to usage concerns

--- lib/MLS/Resource/Paragon/sandicor2/Config/Property.pm
+++ lib/MLS/Resource/Paragon/sandicor2/Config/Property.pm
@@ -58,6 +58,8 @@ $MLS::Config::MV_ACTIVE_COLS = q|
   FROM sandicor2."Property"
 |;
 
+exit if (! MLS::Resource::Utils::is_peak_time());
+
 # RETS Resource Classes
 my $search = MLS::Resource::Utils::get_search_interval();
 %MLS::Config::CLASSES = (
